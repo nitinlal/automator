71cd73fb14a9ab48f88e0ac58654ce75ef62787e
Nitin Lal <nklal4@gmail.com>
2023-07-31 19:11:33 -0700

    update

--- test.sh
+++ test.sh
@@ -12,15 +12,21 @@ if [ $? -ne 0 ]; then
 fi
 
 for dir in "$1"/*; do
-    if [ -f "$dir" ]; then
+    if [ ! -d "$dir" ]; then
+        echo "Skipping $dir because it is not a directory"
         continue
     fi
+
     echo "Processing directory: $dir"
     cd "$dir" || continue
     echo "Running npm install under $dir ..."
 
-    # Adding the directory name and some new lines to the file
-    echo -e "\n\nDirectory: $dir\n" >>"$root_dir/$file_name"
+    if [ ! -f "package.json" ]; then
+        echo "No package.json file found, skipping ..."
+        continue
+    fi
+
+    echo -e "\e[33m$dir\e[0m" >>"$root_dir/$file_name"
     npm install >>"$root_dir/$file_name" 2>&1
 
     echo "Returning to the root directory ..."
